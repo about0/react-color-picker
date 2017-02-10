f09f01d791a2728400e0f425013d0bfb341d944f
about0 <ghostautomatic@gmail.com>
2017-02-11 00:32:28 +0200

    Made every single click to generate random color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const Square = React.createClass({
   render() {
     const squareStyle = {
       height: 150,
-      backgroundColor: this.props.color
+      backgroundColor: this.props.backgroundColor
     };
     return (
       <div style={squareStyle}>
@@ -14,19 +14,7 @@ const Square = React.createClass({
   }
 });
 
-
 class Label extends React.Component {
-  constructor(prop) {
-    super(prop);
-
-    this._handelColorPick = this._handelColorPick.bind(this);
-  };
-
-  _handelColorPick() {
-    this.setState = {
-      backgroundColor: "green"
-    }
-  };
 
   render() {
     const labelStyle = {
@@ -37,19 +25,33 @@ class Label extends React.Component {
     };
 
     return (
-      <p style={labelStyle} onClick={this._handelColorPick}>{this.props.color}</p>
+      <p style={labelStyle} >{this.props.backgroundColor}</p>
     );
   }
 }
 
+
+
 class Card extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {backgroundColor: "blue"};
 
-    this.state = {
-      backgroundColor: "blue"
+   this._handelRandomColor = this._handelRandomColor.bind(this);
+  };
+
+
+  _handelRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let o = 0; o < 6; o++) {
+      color += letters[Math.floor(Math.random() * 16)]
     }
-  }
+    this.setState({
+      backgroundColor: color
+    });
+  };
 
   render() {
     const cardStyle = {
@@ -62,20 +64,20 @@ class Card extends React.Component {
     };
 
     return (
-      <div style={cardStyle}>
-        <Square color={this.props.color}/>
-        <Label color={this.props.color}/>
+      <div style={cardStyle} onClick={this._handelRandomColor}>
+        <Square {...this.state} />
+        <Label {...this.state}/>
       </div>
     );
   }
 }
 
 class App extends Component {
+
   render() {
     return (
       <div>
-        <Card color="#FF6123"/>
-
+        <Card />
       </div>
     );
   }
