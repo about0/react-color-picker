5b55267f6178b00fd2f77a7a16c73f1930ebb656
about0 <ghostautomatic@gmail.com>
2017-02-11 14:52:33 +0200

    branch with timer

--- src/App.js
+++ src/App.js
@@ -1,74 +1,50 @@
 import React, {Component} from 'react';
 
-
-const Square = React.createClass({
-  render() {
-    const squareStyle = {
-      height: 150,
-      backgroundColor: this.props.backgroundColor
+class LightningCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      strikes: 0
     };
-    return (
-      <div style={squareStyle}>
-      </div>
-    );
+
+    this.timerTick = this.timerTick.bind(this);
   }
-});
 
-class Label extends React.Component {
+  timerTick() {
+    this.setState({
+      strikes: this.state.strikes + 10
+    })
+  }
+
+  componentDidMount() {
+    setInterval(this.timerTick, 100)
+  }
+
 
   render() {
-    const labelStyle = {
-      fontFamily: "sans-serif",
-      fontWeight: "bold",
-      padding: 13,
-      margin: 0
-    };
-
     return (
-      <p style={labelStyle} >{this.props.backgroundColor}</p>
-    );
+      <h1>{this.state.strikes}</h1>
+    )
   }
 }
 
-
-
-class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {backgroundColor: "blue"};
-
-   this._handelRandomColor = this._handelRandomColor.bind(this);
-  };
-
-
-  _handelRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-
-    for (let o = 0; o < 6; o++) {
-      color += letters[Math.floor(Math.random() * 16)]
-    }
-    this.setState({
-      backgroundColor: color
-    });
-  };
-
+class CounterDisplay
+  extends React.Component {
   render() {
-    const cardStyle = {
-      height: 200,
-      width: 150,
-      padding: 0,
-      backgroundColor: "#FFF",
-      WebkitFilter: "drop-shadow(0px 0px 5px #666)",
-      filter: "drop-shadow(0px 0px 5px #666)"
+    const divStyle = {
+      width: 250,
+      textAlign: 'center',
+      backgroundColor: "black",
+      padding: 40,
+      fontFamily: "sans-serif",
+      color: "#999"
     };
-
     return (
-      <div style={cardStyle} onClick={this._handelRandomColor}>
-        <Square {...this.state} />
-        <Label {...this.state}/>
+      <div style={divStyle}>
+        <LightningCounter />
       </div>
-    );
+    )
+
   }
 }
 
@@ -77,7 +53,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Card />
+        <CounterDisplay />
       </div>
     );
   }
